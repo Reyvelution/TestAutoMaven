ebb88b4e3d8ef5c0aefb85423e4965676a5a5d8a
Sreya Nair <sreyanair@Sreyas-MacBook-Pro.local>
2025-06-04 13:21:29 +0530

    First Local Repo Commit

---
+++ demo/src/test/java/CalcTest.java
@@ -0,0 +1,20 @@
+import org.junit.Test;
+import org.junit.Assert;
+
+public class CalcTest {
+
+    @Test
+    public void addTwoNumbers_1() {
+        int a = 2 + 2;
+   
+        Assert.assertTrue(a == 4);
+
+    }
+
+    @Test
+    public void addTwoNumbers_2() {
+        int a = 3 + 5;
+        Assert.assertTrue(a == 7);
+
+    }
+}
